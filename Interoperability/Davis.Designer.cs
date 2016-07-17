3125fadd92d17eca0c2dd9e528acc3047824d5ab
Davis <davis.wu@mail.utoronto.ca>
2016-07-17 19:15:05 -0400

    Added interface

---
+++ Interoperability/Davis.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace interoperability
+{
+    partial class Davis
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.uniqueTelUploadText = new System.Windows.Forms.TextBox();
+            this.avgTelUploadText = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Unique telemetry upload rate:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(181, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Average telemetry upload rate:";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 43.61949F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 56.38051F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.uniqueTelUploadText, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.avgTelUploadText, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(431, 55);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // uniqueTelUploadText
+            // 
+            this.uniqueTelUploadText.Location = new System.Drawing.Point(191, 3);
+            this.uniqueTelUploadText.Name = "uniqueTelUploadText";
+            this.uniqueTelUploadText.ReadOnly = true;
+            this.uniqueTelUploadText.Size = new System.Drawing.Size(237, 20);
+            this.uniqueTelUploadText.TabIndex = 2;
+            this.uniqueTelUploadText.Text = "Hz";
+            // 
+            // avgTelUploadText
+            // 
+            this.avgTelUploadText.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.avgTelUploadText.Location = new System.Drawing.Point(191, 30);
+            this.avgTelUploadText.Name = "avgTelUploadText";
+            this.avgTelUploadText.ReadOnly = true;
+            this.avgTelUploadText.Size = new System.Drawing.Size(237, 20);
+            this.avgTelUploadText.TabIndex = 3;
+            this.avgTelUploadText.Text = "Hz";
+            // 
+            // Davis
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 310);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Davis";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Davis";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox uniqueTelUploadText;
+        private System.Windows.Forms.TextBox avgTelUploadText;
+
+
+
+    }
+}
---
+++ Interoperability/Davis.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace interoperability
+{
+    public partial class Davis : Form
+    {
+        public Davis()
+        {
+            InitializeComponent();
+        }
+
+        public void setUniqueTelUploadText(string text)
+        {
+            try
+            {
+                if (this.uniqueTelUploadText.InvokeRequired)
+                {
+                    this.uniqueTelUploadText.BeginInvoke((MethodInvoker)delegate()
+                    {
+                        this.uniqueTelUploadText.Text = text;
+                    });
+                    return;
+                }
+                uniqueTelUploadText.Text = text;
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("FAILED " + text); 
+            }
+        }
+
+        public void setAvgTelUploadText(string text)
+        {
+            try
+            {
+                if (this.avgTelUploadText.InvokeRequired)
+                {
+                    this.avgTelUploadText.BeginInvoke((MethodInvoker)delegate()
+                    {
+                        this.avgTelUploadText.Text = text;
+                    });
+                    return;
+                }
+                avgTelUploadText.Text = text;
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("FAILED " + text); 
+            }
+        }
+    }
+}
